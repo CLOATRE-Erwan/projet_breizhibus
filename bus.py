c154d83bb7ed3391d0d176c78389871b546b2fa2
CLOATRE-Erwan <73162784+CLOATRE-Erwan@users.noreply.github.com>
2020-12-20 15:30:33 +0100

    Delete bus.py

--- bus.py
+++
@@ -1,7 +0,0 @@
-class bus:
-    def __init__(self, id_bus, numero, immatriculation, nombre_place, id_ligne):
-        self.id_bus = id_bus
-        self.numero = numero
-        self.immatriculation = immatriculation
-        self.nombre_place = nombre_place
-        self.id_ligne = id_ligne
